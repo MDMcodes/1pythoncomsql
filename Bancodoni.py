0565ab6c190fc8a378191225561fe3b690b388e5
MDMcodes <matheusdonadelmarques08@gmail.com>
2024-10-08 15:11:23 -0300

    commit4

--- Bancodoni.py
+++ Bancodoni.py
@@ -40,12 +40,39 @@ cursor = conexao_banco.cursor()
 
 #READ
 
+# comando_sql = 'SELECT * FROM produtos'
+# cursor.execute(comando_sql)
+# dados_tabela = cursor.fetchall()
+# print(dados_tabela[0][0])
+# print(dados_tabela[1][1])
+# print(dados_tabela[2][2])
+
+# for i in dados_tabela:
+#     print(f'ID: {i[0]}  PRODUTO {i[1]}   VALOR: {i[2]}')
+
+
+#-----------------------------------------------------------------------
+#Verificar se o id esta cadastrado no update
+
+input_id = int(input('Digite o ID do produto que será alterado: '))
 comando_sql = 'SELECT * FROM produtos'
 cursor.execute(comando_sql)
 dados_tabela = cursor.fetchall()
-print(dados_tabela[0][0])
-print(dados_tabela[1][1])
-print(dados_tabela[2][2])
+for dados in dados_tabela:
+    if dados[0] == input_id:
+        esc = int(input(f'Deseja alterar o (1)nome ou o (2)valor?: '))
+        if esc == 1:
+            nome = input("Digite o novo nome do produto: ")
+            comando_sql = f'UPDATE produtos SET nome_produto = "{nome}" WHERE id_produtos  = {input_id}'
+            cursor.execute(comando_sql)
+            conexao_banco.commit()
+        elif esc == 2:
+            valor = float(input("Digite o novo valor do produto: "))
+            comando_sql = f'UPDATE produtos SET valor_produto = "{valor}" WHERE id_produtos  = {input_id}'
+            cursor.execute(comando_sql)
+            conexao_banco.commit()
+    else:
+        print('ID não encontrado')
 
-for i in dados_tabela:
-    print(f'ID: {i[0]}  PRODUTO {i[1]}   VALOR: {i[2]}')
+
+
